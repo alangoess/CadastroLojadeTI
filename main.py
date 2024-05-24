9f6c0ef4c96fc6268dd16ef2c168c852f68955d9
Alan Gabriel <depzz123@gmail.com>
2024-05-24 00:38:28 -0300

    Cadastro Loja de TI

--- main.py
+++ main.py
@@ -1,35 +1,66 @@
 import sqlite3
 from time import sleep
 
+banco = sqlite3.connect('cadastro_clientes.db')
+cursor = banco.cursor()
+
 print('Olá, seja bem-vindo à Clínica do Computador!')
 sleep(1)
 
-opcoes = input('Escolha as opções abaixo: \n [1] Cadastro \n [2] Avaliação de Máquina \n [3] Feedback \n ')
+opcoes = input('Escolha as opções abaixo: \n [1] Avaliação de Máquina \n [2] FeedBack \n ')
 
-if opcoes == '1':
-    print('Para criar o seu cadastro, precisamos colher algumas informações!')
+if opcoes == '2':
 
-    Nome = input('Digite o seu nome completo: ')
-    DataNasc = input('Digite a sua data de nascimento: ')
-    Cpf = input('Digite o seu CPF: ')
+  feedback = input('Digite seu feedback')
 
-    print(f"Obrigado pelas informações {Nome}")
-
-elif opcoes == '2':
+else:
+    cadastro = input('Você já possui cadastro? \n [1] Sim [2] Não \n ')
+if  cadastro == '1':
+    
+    NomeCliente = input('Qual é o nome do cliente?')
     problema = input('Qual problema a sua máquina está apresentando? ')
-    mexeu = input('Você tentou consertar por conta própria? Se sim, o que você fez? ')
+    mexeu = input('A máquina já foi aberta alguma vez? ')
     vezes_que_aconteceu = input('Já aconteceu esse tipo de problema alguma vez? ')
-
+    
     sleep(1)
 
     print('Entendi... O problema apresentado foi anotado! Você terá que deixar a sua máquina com o técnico. O prazo da análise dura em torno de 5 dias úteis.')
 
     confirmacao = input('Deseja confirmar a Analise da sua máquina? ')
     if confirmacao == 'sim':
+        cursor.execute("CREATE TABLE IF NOT EXISTS OrdemDeServiço(Nome text, Problema text, aberta text, Deuproblema text)")
+        cursor.execute(f"INSERT INTO OrdemDeServiço VALUES('{NomeCliente}', '{problema}', '{mexeu}', '{vezes_que_aconteceu}')")
+        banco.commit()
+        
         print('Agendamento feito com sucesso!')
     else:
         print('Que pena, conte para nós o que aconteceu na opção feedback!')
 
-else:
-    feedback = input('Nos informe o seu feedback, garanto que será muito útil para a nossa empresa!')
-    print('Muito Obrigado pelo seu feedback, iremos sempre tentar melhorar o nosso atendimento para satisfazer nossos clientes!')    
+
+elif cadastro == '2':
+    print('Para criar o seu cadastro, precisamos colher algumas informações!')
+
+    Nome = input('Digite o seu nome completo: ')
+    Cpf = input('Digite o seu CPF: ')
+    Categoria = input('Técnico ou Consumidor Final...')
+    print(f"Cadastro criado com sucesso!")
+   
+    #cursor.execute("DROP TABLE IF EXISTS cadastro")
+    cursor.execute("CREATE TABLE IF NOT EXISTS cadastro(Nome text, CPF integer, Categoria text)")
+    cursor.execute(f"INSERT INTO cadastro VALUES('{Nome}',{Cpf},'{Categoria}' )")
+    banco.commit()
+
+
+
+
+
+
+
+
+ 
+
+    
+
+
+  
+    
